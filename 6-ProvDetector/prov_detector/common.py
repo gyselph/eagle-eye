185e7c9ca1eb6eb47ff446a581cb6530d4d0fd9a
Philipp Gysel <gyselph@gmail.com>
2025-03-11 10:48:40 +0100

    some cleanup in common.py

--- 6-ProvDetector/prov_detector/common.py
+++ 6-ProvDetector/prov_detector/common.py
@@ -3,26 +3,26 @@ from pathlib import Path
 
 
 SEPERATOR = ","
-BENIGN = "benign"
-MALICIOUS = "malicious"
+BENIGN_MARKER = "benign"
+MALICIOUS_MARKER = "malicious"
 BENIGN_LABEL = 0
 MALICIOUS_LABEL = 1
 
 
 def read_all_events(event_folder):
     csv_files = list(Path(event_folder).rglob("*.csv"))
-    csv_files = [str(path).replace("\\", "/") for path in csv_files]
+    csv_files = [str(x) for x in csv_files]
     graph_labels = {}
-    list_of_dataframes, list_of_events, list_of_graph_ids = [], [], []
+    list_of_dataframes = []
     print("Start reading in all events from {}".format(event_folder))
     for i in range(len(csv_files)):
-        tmp_dataframe, tmp_events, tmp_graph_ids = read_csv_file(csv_files[i], i)
+        tmp_dataframe = read_csv_file(csv_files[i], i)
         list_of_dataframes.append(tmp_dataframe)
-        list_of_events.extend(tmp_events)
-        list_of_graph_ids.extend(tmp_graph_ids)
-        label = BENIGN_LABEL if BENIGN in csv_files[i] else MALICIOUS_LABEL
+        label = BENIGN_LABEL if BENIGN_MARKER in csv_files[i] else MALICIOUS_LABEL
         graph_labels[i] = label
     one_big_dataframe = pd.concat(list_of_dataframes)
+    list_of_events = one_big_dataframe.values.tolist()
+    list_of_graph_ids = list(range(len(csv_files)))
     print("Finished reading in all events")
     return one_big_dataframe, list_of_events, list_of_graph_ids, graph_labels
 
@@ -35,6 +35,4 @@ def read_csv_file(event_file, graph_id):
     df_events['action'] = df_events['action'].str.strip('" \n\t')
     df_events['sourceType'] = df_events['sourceType'].str.strip('" \n\t')
     df_events['destinationType'] = df_events['destinationType'].str.strip('" \n\t')
-    parsedList = df_events.values.tolist()
-    uniqueGraphNames = sorted(list(df_events.graphId.unique()))
-    return df_events, parsedList, uniqueGraphNames
+    return df_events
