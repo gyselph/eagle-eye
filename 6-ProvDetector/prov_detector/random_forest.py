1f062b3f60423a1ef9fcccf21f83665a53779866
Philipp Gysel <gyselph@gmail.com>
2025-03-11 16:08:06 +0100

    clean up rf.py

--- 6-ProvDetector/prov_detector/random_forest.py
+++ 6-ProvDetector/prov_detector/random_forest.py
@@ -1,27 +1,30 @@
-import numpy as np
-import os
-from joblib import dump, load
+"""Train a random forest to detect malware based on rare path embeddings."""
+
 from sklearn.ensemble import RandomForestClassifier
+import numpy as np
 
-
-def train_rf(data, labels, num_estimators, max_depth):
+def train_rf(training_data: np.ndarray, labels: np.ndarray, num_estimators: int, max_depth: int) -> RandomForestClassifier:
     """
-    Train a random forest for supervised classification. Inputs: Doc2Vec embeddings. Outputs: binary prediction.
+    Train a random forest for supervised classification.
+    Inputs: Doc2Vec embeddings. Outputs: binary prediction.
+
+    :param training_data: Training data of shape (number of rare paths, embedding dimension)
+    :param labels: Binary labels of shape (number of rare paths,)
+    :param num_estimators: Hyper parameter: number of trees for the forest
+    :param max_depth: Hyper parameter: the maximal depth per tree
     """
     rf = RandomForestClassifier(n_estimators = num_estimators, max_depth = max_depth, n_jobs = -1)
     print("Start Random Forest training ...")
-    rf.fit(data, labels)
+    rf.fit(training_data, labels)
     print("Random Forest training has finished")
     return rf
 
 
-def test_rf(data, model):
-    return model.predict(data)
-
-
-def load_rf_model(model_file):
-    return load(model_file)
-
-
-def store_rf_model(model, model_file):
-    dump(model, model_file) 
+def test_rf(test_data: np.ndarray, model: RandomForestClassifier) -> np.ndarray:
+    """Run inference on the trained random forest.
+    
+    :param test_data: Test data of shape (number of rare paths, embedding dimension)
+    :param model: The trained random forest
+    :return: The prediction data of shape (number of rare paths,)
+    """
+    return model.predict(test_data)
